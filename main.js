48963eab2476ad99fd1bf84c7171642d48cadd6d
Shaha2258 <147894356+Shaha2258@users.noreply.github.com>
2024-01-03 13:35:45 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+function openSidebar(){
+    document.querySelector(".sidebar").style.width = "250px";
+}
+
+function closeSidebar(){
+    document.querySelector(".sidebar").style.width = "0px";
+}
+
